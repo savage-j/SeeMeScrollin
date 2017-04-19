4bb4383283dc55e2bf71297320ce442ceac296ff
Jordan <savage@savagej.com>
2017-04-18 21:34:36 -0700

    add podfile

---
+++ Pods/Target Support Files/AlamofireRSSParser/AlamofireRSSParser-umbrella.h
@@ -0,0 +1,9 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "AlamofireRSSParser.h"
+
+FOUNDATION_EXPORT double AlamofireRSSParserVersionNumber;
+FOUNDATION_EXPORT const unsigned char AlamofireRSSParserVersionString[];
+
